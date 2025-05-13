45b3009d033fc3db91408fe3533374c107f952e5
PenNy <pkazenny06@gmail.com>
2025-05-13 23:00:57 +0300

    Update pack.hpp

--- pack.hpp
+++ pack.hpp
@@ -5,26 +5,26 @@ namespace PACK {//PACK=посылка
 class pack {
     std::string _sendname;//имя отправителя
     std::string _getname;//имя получателя
-    std::string _from; //откуда отправляется
-    std::string _to;//куда отправляется
+    int _from; //откуда отправляется
+    int _to;//куда отправляется
     int _weight;//вес посылки
     int _id; //трек номер
 
 public:
     pack() = default;
-    pack(std::string sendname, std::string getname, std::string from, std::string to, int weight, int id);
+    pack(std::string sendname, std::string getname, int from, int to, int weight, int id);
     std::string sendname();
     std::string getname();
-    std::string from();
-    std::string to();
+    int from();
+    int to();
     int weight();
     int id();
     friend std::ostream& operator << (std::ostream & out, const pack& p);
     friend std::istream& operator >> (std::istream & in, pack& p);
     void setGetname(const std::string& getname) { _getname = getname; }
     void setSendname(const std::string& sendname) { _sendname = sendname; }
-    void setFrom(const std::string& from) { _from = from; }
-    void setTo(const std::string& to) { _to = to; }
+    void setFrom(int from) { _from = from; }
+    void setTo(int to) { _to = to; }
     void setWeight (int weight) { _weight = weight; }
     void setId(int id) { _id = id; }
 };
