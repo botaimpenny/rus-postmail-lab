2eb9f622a86ed5a0b6764092f10ee1f3eb5e1df2
milanacharonova <popelnitckiy2@gmail.com>
2025-05-16 11:58:38 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,63 +1,45 @@
-/**
- * @file main.cpp
- * @brief Главный файл программы, реализующий пользовательский интерфейс для работы с почтовой системой.
- */
 #include "pack.hpp"
+#include <iostream>
 using namespace std;
 using namespace PACK;
-/**
- * @brief Точка входа в программу.
- * @return Код завершения программы.
- */
+
 int main() {
     post post;
     pack* arr = nullptr;
     int count = 0;
-    int a;
 
+    // Загрузка данных
+    arr = load(count);
+
+    int a;
     do {
-        cout << "1. Отправить посылку\n"
-            << "2. Добавить отделение\n"
-            << "3. Удалить отделение\n"
-            << "4. Узнать информацию о отделении\n"
-            << "5. Отследить посылку по трек номеру\n"
-            << "6. Выдать посылку\n"
-            << "7. Пропуск времени\n"
-            << "0. Выйти\n";
+        cout << "\n--- Меню ---\n"
+             << "1. Отправить посылку\n"
+             << "2. Добавить отделение\n"
+             << "3. Удалить отделение\n"
+             << "4. Показать информацию об отделениях\n"
+             << "5. Рассчитать время доставки\n"
+             << "6. Выдать посылку\n"
+             << "7. Пропустить день\n"
+             << "0. Выйти\n"
+             << "Выберите действие: ";
         cin >> a;
 
         switch (a) {
-        case 1:
-            add(&arr, &count);
-            break;
-        case 2:
-            post.addpost();
-            break;
-        case 3:
-            post.delpost(arr, count);
-            break;
-        case 4:
-            post.display();
-            break;
-        case 5:
-            cout << "Введите трек номер: ";
-            int id;
-            cin >> id;
-            post.distance(arr, count, id);
-            break;
-        case 6:
-            give(arr, &count);
-            break;
-        case 7:
-            advanceTime(arr, count);
-            break;
-        case 0:
-            cout << "Выход из программы." << endl;
-            save(arr, count);
-            delete[] arr;
-            break;
-        default:
-            cout << "Неверный ввод. Пожалуйста, попробуйте снова." << endl;
+            case 1: add(&arr, &count); break;
+            case 2: post.addpost(); break;
+            case 3: post.delpost(arr, count); break;
+            case 4: post.display(); break;
+            case 5: post.distance(arr, count); break;
+            case 6: give(arr, &count); break;
+            case 7: advanceTime(arr, count); break;
+            case 0:
+                cout << "Выход. Сохранение данных..." << endl;
+                save(arr, count);
+                delete[] arr;
+                break;
+            default:
+                cout << "Неверный выбор. Повторите." << endl;
         }
     } while (a != 0);
 
